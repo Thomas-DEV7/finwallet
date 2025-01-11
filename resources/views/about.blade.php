eef10679ec1f91f28123bc10bbff747c836a834b
Thomas Bastos <thomas.felip16@gmail.com>
2025-01-11 00:45:31 -0300

    fix: add link to my profile in linkedin

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,15 +11,20 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h1 class="text-2xl font-bold mb-4">Sobre Nós</h1>
                     <p>
-                        Bem-vindo ao FinWallet! Somos uma plataforma focada em oferecer a melhor experiência de gestão financeira
+                        Bem-vindo ao FinWallet! Somos uma plataforma focada em oferecer a melhor experiência de gestão
+                        financeira
                         para nossos usuários. Nosso objetivo é tornar suas finanças simples, acessíveis e seguras.
                     </p>
                     <p class="mt-4">
-                        Nossa missão é ajudar você a gerenciar suas finanças com eficiência e transparência, seja transferindo
+                        Nossa missão é ajudar você a gerenciar suas finanças com eficiência e transparência, seja
+                        transferindo
                         dinheiro, realizando depósitos ou monitorando suas transações.
                     </p>
                     <p class="mt-4">
                         Entre em contato conosco caso tenha dúvidas ou sugestões!
+                    </p>
+                    <p class="mt-4">
+                        Desenvolvido por <a href="https://www.linkedin.com/in/thomasbastos/">Thomas Bastos</a>
                     </p>
                 </div>
             </div>
